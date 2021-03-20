0a340cf08b43dfbb3f3a601500605ba5fb57823c
unknown <valynaceres@gmail.com>
2021-03-20 18:45:56 +0400

    Message

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,13 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import ProductHero from '../ProductHero';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <ProductHero />
       <Cards />
       <Footer />
     </>
