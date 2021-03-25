7d4a3b0a0ccfb31b3904b433ec6f03382c01f7a5
valery-aceres <79005386+valery-aceres@users.noreply.github.com>
2021-03-25 23:40:40 +0400

    updated margins of cart page

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -24,15 +24,21 @@ export class Cart extends Component {
                         
                         cart.map(item =>(
                             <div className="details cart" key={item._id}>
-                                <img src={item.src} alt=""/>
-                                <div className="box">
-                                    <div className="row">
+                                <img style={{margin:"20px"}} src={item.src} alt=""/>
+                                <div className="box" >
+                                    <div className="row" style={{marginTop:"100px"}} >
                                         <h2>{item.brand}</h2>
                                         <span>AED {item.price * item.count}</span>
                                     </div>
                                     <Colors colors={item.colors}/>
-                                    
-                                    <p>{item.title}</p>
+                                    <ul style={{listStyleType: "none", marginBottom:"30px", padding:"0"}}>
+                                        <li>OS: {item.os}</li>
+                                        <li>Brand: {item.brand}</li>
+                                        <li>RAM: {item.ram}</li>
+                                        <li>Product Dimensions: {item.dimension}</li>
+                                        <li>Item weight: {item.weight}</li>
+                                        <li>What's in the box: {item.box}</li>
+                                    </ul>
                                     <div className="amount">
                                         <button className="count" onClick={() => reduction(item._id)}> - </button>
                                         <span>{item.count}</span>
